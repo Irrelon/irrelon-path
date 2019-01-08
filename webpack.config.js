e20c6316792da928fad4eb5ebc4def98c918918b
Rob Evans <rob@irrelon.com>
2019-01-08 17:44:14 +0000

    Removed grunt, added webpack, updated path splitting functionality to allow escaped path components

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const webpack_rules = [];
+
+const webpackOption = {
+	entry: "./builds/all.js",
+	output: {
+		path: path.resolve(__dirname, 'js', "dist"),
+		filename: "Path.js",
+	},
+	module: {
+		rules: webpack_rules
+	}
+};
+
+let babelLoader = {
+	test: /\.js$/,
+	exclude: /(node_modules|bower_components)/,
+	use: {
+		loader: "babel-loader",
+		options: {
+			presets: ["@babel/preset-env"]
+		}
+	}
+};
+
+webpack_rules.push(babelLoader);
+module.exports = webpackOption;
